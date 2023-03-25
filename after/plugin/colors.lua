0b1f0768ac651453b9e3d852274c54c557c38e86
Chris Hammock <chris@crshmk.com>
2023-03-25 09:48:04 +0700

    brighten line numbers

---
+++ after/plugin/colors.lua
@@ -0,0 +1,5 @@
+-- line numbrs
+-- #C7CEE9 
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#B0B0B1' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='white' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#B0B0B1'})
